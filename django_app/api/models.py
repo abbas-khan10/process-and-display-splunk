e11b0c6985e8876ae5229bbd84dc2ff837cf1077
Stephane Torres <stephane.torres@madetech.com>
2024-09-24 15:12:53 +0100

    introduce models

--- django_app/api/models.py
+++ django_app/api/models.py
@@ -3,6 +3,19 @@ from pydantic import BaseModel
 
 class DocumentMigration(BaseModel):
     successful: bool = None
+    reason:str = None
+
+class Attachment(BaseModel):
+    clinical_type: str = None
+    mine_type: str = None
+    size_bytes: int = None
+
+class Payload(BaseModel):
+    payload: Attachment = None
+
+class Message(BaseModel):
+    document_migration: DocumentMigration = None
+    payload: Payload = None
 
 
 
