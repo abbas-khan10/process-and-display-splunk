bc1be7512b7d10df5bf040637f6c6c8313c9c036
abbas-khan10 <abbas.khan10@nhs.net>
2024-09-24 13:55:55 +0100

    add api

---
+++ django_app/migrate_db.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+folder_path = "data"
+json_files = [f for f in os.listdir(folder_path) if f.endswith(".json")]
+
+data_array = []
+
+for file_name in json_files:
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+        data_array.append(data)
